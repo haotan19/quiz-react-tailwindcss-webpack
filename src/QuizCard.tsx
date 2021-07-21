166ecfd9249dbd641c169086cb1774f03436b593
Tim_Tan <80441177+haotan19@users.noreply.github.com>
2021-07-22 07:46:52 +0800

    Purge working

--- src/QuizCard.tsx
+++ src/QuizCard.tsx
@@ -11,7 +11,7 @@ const QuizCard: React.FC<Props> = ({ children }) => {
   return (
     <div
       style={style}
-      className="bg-white rounded-md overflow-hidden shadow-xl w-full h-full max-w-sm sm:max-w-xl md:max-w-3xl mx-auto container px-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      className="bg-white rounded-md overflow-hidden shadow-xl w-full h-full max-w-sm sm:max-w-xl md:max-w-3xl mx-auto  px-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
     >
       {children}
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,58 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = {
-  entry: "./src/_app.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /.s?css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    "postcss-preset-env",
-                    {
-                      // Options
-                    },
+module.exports = (env, options) => {
+  const isDevelopment = options.mode !== "production";
+
+  process.env.NODE_ENV = options.mode;
+
+  return {
+    entry: "./src/_app.tsx",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      "postcss-preset-env",
+                      {
+                        // Options
+                      },
+                    ],
                   ],
-                ],
+                },
               },
             },
-          },
-        ],
-      },
-    ],
-  },
-  // optimization: {
-  //   minimizer: [
-  //     // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-  //     // `...`,
-  //     new CssMinimizerPlugin(),
-  //   ],
-  // },
-  plugins: [new MiniCssExtractPlugin()],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+          ],
+        },
+      ],
+    },
+    // optimization: {
+    //   minimizer: [
+    //     // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+    //     // `...`,
+    //     new CssMinimizerPlugin(),
+    //   ],
+    // },
+    plugins: [new MiniCssExtractPlugin()],
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+  };
 };
