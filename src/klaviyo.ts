bc5a84efbccb8cb55476a33a758bb1118d36ec1a
Tim_Tan <80441177+haotan19@users.noreply.github.com>
2021-08-09 18:13:33 +0800

    Klaviyo update

--- src/klaviyo.ts
+++ src/klaviyo.ts
@@ -9,7 +9,7 @@ export const submitAnswers = (data: string[][]) => {
     const obj = {};
     data.map((item) => (obj[item[0]] = item[1]));
 
-    _learnq.push(["track", "Quiz Answers", obj]);
+    _learnq.push(["track", "Took The Quiz", obj]);
   } catch (err) {
     console.error(err);
   }
@@ -17,18 +17,18 @@ export const submitAnswers = (data: string[][]) => {
 
 export const submitAddToCart = (productData: any) => {
   try {
-    if(productData){
+    if (productData) {
       const obj = {
-        Brand:productData.vendor,
+        Brand: productData.vendor,
         CompareAtPrice: productData.compare_at_price,
         ImageURL: productData.images[0],
         Name: productData.title,
         Price: productData.price * 100,
-        ProductID: productData.id
-      }
+        ProductID: productData.id,
+      };
       _learnq.push(["track", "Added to Cart", obj]);
     }
   } catch (err) {
     console.error(err);
-  } 
-}
+  }
+};
