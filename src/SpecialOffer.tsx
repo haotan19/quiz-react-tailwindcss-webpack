24b6443c2466c69ff0e74cd983f42f92a247750b
Hao Tan <80441177+haotan19@users.noreply.github.com>
2021-08-11 23:12:56 +0800

    Final update

--- src/SpecialOffer.tsx
+++ src/SpecialOffer.tsx
@@ -22,14 +22,16 @@ const SpecialOffer = ({ SOActive, setSOActive }: Props) => {
     return (
       <div className="w-full h-full bg-white relative " style={zIndex}>
         <div className="quiz-so__image h-1/2"></div>
-        <div className="w-full flex justify-center transform -translate-y-1/2">
+        {/* <div className="w-full flex justify-center transform -translate-y-1/2">
           <div className="inline-block border text-xl px-7 py-2 bg-white">
             SPECIAL OFFER
           </div>
-        </div>
+        </div> */}
         <div className="flex flex-col justify-center items-center h-2/5">
           {/* <p className="text-xl">Sign up for our newsletter &</p> */}
-          <h4 className="text-2xl mb-10">What is your email?</h4>
+          <h4 className="text-2xl mb-4 mt-12 normal-case tracking-normal">
+            What is your email?
+          </h4>
           <div className="quiz-newsletter__input-wrapper">
             <input
               className="quiz-newsletter__input text-center"
@@ -43,13 +45,22 @@ const SpecialOffer = ({ SOActive, setSOActive }: Props) => {
                       hover:text-black hover:bg-white border-2 border-opacity-0 hover:border-opacity-100 transition-all
                         quiz-newsletter__next-btn"
             onClick={() => {
-              identify(email);
+              try {
+                identify(email);
+              } catch (err) {
+                console.error(err);
+              }
               setSOActive(false);
             }}
           >
             NEXT
           </button>
-          <button className="quiz-newsletter__skip-btn">Skip for now</button>
+          <button
+            className="quiz-newsletter__skip-btn"
+            onClick={() => setSOActive(false)}
+          >
+            Skip for now
+          </button>
         </div>
         <button
           className="p-2 bg-white absolute top-4 right-0"
